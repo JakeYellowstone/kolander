d92f431fa42806f45f55fad2ed5800253c445be2
Esdras Jonathan <genjakeyellowstone@gmail.com>
2025-06-14 08:30:06 +0000

    Restructuration header

--- interface/src/app/components/header/header.component.ts
+++ interface/src/app/components/header/header.component.ts
@@ -5,31 +5,8 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
+  templateUrl: "./header.component.html",
   styleUrl : "./header.component.css",
   imports: [CommonModule, RouterModule],
-  template: `
-    <header class="header">
-      <div class="header-content">
-        <div class="logo">
-          <h1>
-            <span class="cyber-text">Kolander </span>
-            <span class="edr-text">carbon</span>
-          </h1>
-          <p class="tagline">Security Intelligence Platform</p>
-        </div>
-        
-        <nav class="nav">
-          <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
-          <a routerLink="/analysis" routerLinkActive="active">Analyse</a>
-          <a routerLink="/config" routerLinkActive="active">Config</a>
-        </nav>
-        
-        <div class="status">
-          <div class="status-indicator online"></div>
-          <span>Système connecté</span>
-        </div>
-      </div>
-    </header>
-  `,
 })
 export class HeaderComponent {}
